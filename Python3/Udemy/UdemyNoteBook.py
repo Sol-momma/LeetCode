ddb185ffa5c1cae39c4e59a011d7bcc9440511f7
So Momma <sol0608sol@gmail.com>
2025-05-16 11:08:57 +0800

    feat:udemy

---
+++ Python3/Udemy/UdemyNoteBook.py
@@ -0,0 +1,12 @@
+dict1 = {
+    'value' : 11
+}
+
+dict2 = dict1
+
+print("Before value is updated: ")
+print(dict1)
+print(dict2)
+
+print("\ndict1 is points to : ",id(dict1))
+print("dict2 is points to : ",id(dict2))
