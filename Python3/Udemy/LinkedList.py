c537f97358fbec0647d83ad0de7f1c357e253b3d
So Momma <sol0608sol@gmail.com>
2025-05-21 19:13:11 +0800

    feat: algorithm study

--- Python3/Udemy/LinkedList.py
+++ Python3/Udemy/LinkedList.py
@@ -1,2 +1,17 @@
 class Node:
-    def
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_Node = Node(value)
+        self.head = new_Node
+        self.tail = new_Node
+        self.length = 1
+
+my_linked_list = LinkedList(4)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
